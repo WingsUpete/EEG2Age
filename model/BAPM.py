da8813ef79302823d21f79cf8d186ea8ae2d69c8
Peter S <petershen815@126.com>
2021-12-20 00:40:53 +0800

    [MODEL] Test again to set the reference value to 50

--- model/BAPM.py
+++ model/BAPM.py
@@ -60,7 +60,7 @@ class BrainAgePredictionModel(nn.Module):
         pred = self.tranAttLayer(spatTempFeat)
         del spatTempFeat
 
-        return pred * 100
+        return pred * 50
 
 
 if __name__ == '__main__':
