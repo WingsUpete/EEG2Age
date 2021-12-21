703aaed9b4b19d42b6ace0903dc19931f567af1d
Peter S <petershen815@126.com>
2021-12-21 16:09:57 +0800

    [BASH] Updates run_trainer_example.sh

--- run_trainer_example.sh
+++ run_trainer_example.sh
@@ -3,23 +3,23 @@ ulimit -n 65535
 
 ### Preprocess ###
 cd preprocess/
-python GraphBuilder.py -d ../data/EEG_age_data_raw/ -c 4 -s 4 -o ../data/EEG_age_data/
+python GraphBuilder.py -d ../data/EEG_age_data_raw/ -c 4 -s 16 -o ../data/EEG_age_data_s16/
 python GraphCustomizer.py -g ../data/D_lobes.txt -o ../data/
 cd ../
 
 
 
 ### BAPM ###
-python Trainer.py -dr data/EEG_age_data/ -c 4 -m train -net BAPM
-#python Trainer.py -dr data/EEG_age_data/ -c 4 -m eval -e model_save/20211219_23_35_13.pth
+python Trainer.py -dr data/EEG_age_data_s16/ -s 16 -c 4 -m train -net BAPM
+#python Trainer.py -dr data/EEG_age_data_s16/ -s 16 -c 4 -m eval -e model_save/20211219_23_35_13.pth
 
 
 
 ##### Baselines #####
 ### FeedForward ###
-#python Trainer.py -dr data/EEG_age_data/ -c 4 -m train -net FeedForward
-#python Trainer.py -dr data/EEG_age_data/ -c 4 -m eval -e model_save/xxx.pth
+#python Trainer.py -dr data/EEG_age_data_s16/ -s 16 -c 4 -m train -net FeedForward
+#python Trainer.py -dr data/EEG_age_data_s16/ -s 16 -c 4 -m eval -e model_save/xxx.pth
 
 ### GRUNet ###
-#python Trainer.py -dr data/EEG_age_data/ -c 4 -m train -net GRUNet
-#python Trainer.py -dr data/EEG_age_data/ -c 4 -m eval -e model_save/xxx.pth
+#python Trainer.py -dr data/EEG_age_data_s16/ -s 16 -c 4 -m train -net GRUNet
+#python Trainer.py -dr data/EEG_age_data_s16/ -s 16 -c 4 -m eval -e model_save/xxx.pth
